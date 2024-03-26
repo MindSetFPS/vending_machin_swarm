155dd8e5adf021c1696dee8860d0d44a70f39930
Daniel Escobar <daanielescobar@gmail.com>
2024-03-26 10:27:55 -0600

    allow cors

--- main.py
+++ main.py
@@ -13,7 +13,23 @@ from fastapi import FastAPI, Request
 from typing import List
 import json
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 async def root():
